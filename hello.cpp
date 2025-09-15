6f933bdd1214d01e45228c6b7ac3acb5cf167b2d
Abdul Mueed <araja.bscs24seecs@seecs.edu.pk>
2025-09-14 22:42:21 -0700

    Add hello.cpp starter program

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Welcome to DSA Lab 1!" << endl;
+    cout<<"The lab is about learning the setup and use of Git & GitHub"<<endl;
+    cout<<"Created by Abdul Mueed(501166)"<<endl;
+    return 0;
+}
